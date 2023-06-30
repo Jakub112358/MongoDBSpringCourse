62f5d73ecb2abe4f3d983cb977a2a583878d4c5f
Jakub Szymański <jakubszymanski@hotmail.com>
2023-06-30 19:53:23 +0200

    Initial commit

---
+++ src/main/java/com/example/mongodbspringcourse/MongoDbSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbspringcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSpringCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbSpringCourseApplication.class, args);
+    }
+
+}
